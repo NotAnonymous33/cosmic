3c033a1bcd93f8e37b0d8168387fc5cdf6139239
m84948am <arnav.mishra@student.manchester.ac.uk>
2024-04-14 08:22:49 +0100

    minor frontend changes

--- backend/SolarSystemWebApp/src/main/java/com/example/SolarSystemWebApp/controller/LessonController.java
+++ backend/SolarSystemWebApp/src/main/java/com/example/SolarSystemWebApp/controller/LessonController.java
@@ -3,6 +3,7 @@ package com.example.SolarSystemWebApp.controller;
 import com.example.SolarSystemWebApp.model.Lesson;
 import com.example.SolarSystemWebApp.service.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class LessonController {
     @Autowired
     private LessonService lessonService;
 
+    @GetMapping
     public List<Lesson> getLessons() {
         return lessonService.getLessons();
     }
--- frontend/src/Info.jsx
+++ frontend/src/Info.jsx
@@ -1,7 +1,9 @@
 import cards from "./cardinfo.json";
+import lessons from "./lessons/lessons.jsx";
 import Card from "./Card.jsx";
 
 export default function Info() {
+  console.log(lessons);
   const elements = cards.map((card) => (
     <Card
       name={card.name}
