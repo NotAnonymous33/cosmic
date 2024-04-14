0a71b377f2a2d3f02cd2b0c639ac878067c3a280
m84948am <arnav.mishra@student.manchester.ac.uk>
2024-04-14 11:48:32 +0100

    final push

--- frontend/src/Info.jsx
+++ frontend/src/Info.jsx
@@ -3,6 +3,8 @@ import Card from "./Card.jsx";
 import fetchLessons from "./lessons/lessons"; // Import the fetchLessons function
 import "./css/LessonsComponent.css"; // Import custom CSS for styling
 import Lesson from "./Lesson";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const LessonsComponent = () => {
   const [lessons, setLessons] = useState([]);
@@ -12,12 +14,13 @@ const LessonsComponent = () => {
     const fetchLessonsData = async () => {
       try {
         const lessonsData = await fetchLessons();
+        console.log(lessons);
         setLessons(lessonsData); // Update state with fetched lessons
-        setLessonSelected(true)
+        setLessonSelected(true);
       } catch (error) {
         // Handle error if fetching lessons fails
         console.error("Error fetching lessons:", error);
-        setLessonSelected(false)
+        setLessonSelected(false);
       }
     };
 
@@ -25,28 +28,33 @@ const LessonsComponent = () => {
   }, []); // Empty dependency array ensures useEffect runs only once on mount
 
   return (
-      <>
-      { lessonSelected ? (<Lesson lessons={lessons}/>) : (
-        <div className="lessons-container">
-          <h2 className="lessons-title">Cosmic Lessons</h2>
-          <div className="card-container">
-            {lessons.length > 0 ? (
-                lessons.map((lesson) => (
-                    <Card
-                        key={lesson.id}
-                        name={lesson.name}
-                        description={lesson.description}
-                        difficulty={lesson.difficulty}
-                        src={lesson.imageUrl}
-                    />
-                ))
-            ) : (
-                <p>Loading lessons...</p>
-            )}
-          </div>
-        </div>)
-      }
-      </>
+    <>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        theme="dark"
+      />
+      <div className="lessons-container">
+        <h2 className="lessons-title">Cosmic Lessons</h2>
+        <div className="card-container">
+          {lessons.length > 0 ? (
+            lessons.map((lesson) => (
+              <Card
+                key={lesson.id}
+                name={lesson.name}
+                description={lesson.description}
+                difficulty={lesson.difficulty}
+                src={lesson.imageUrl}
+              />
+            ))
+          ) : (
+            <p>Loading lessons...</p>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
